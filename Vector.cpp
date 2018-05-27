010ed4b7dd98f6c952b5b69e2850f51f0c284a5b
caykroyd <christopheraykroyd@hotmail.com>
2018-05-27 22:25:02 +0200

    Vector sum (operator+)

--- Vector.cpp
+++ Vector.cpp
@@ -1,1 +1,11 @@
 #include "Vector.hpp"
+
+Vector Vector::operator+(const Vector& v) const {
+  Vector u;
+
+  u.x = this->x + v.x;
+  u.y = this->y + v.y;
+  u.z = this->z + v.z;
+
+  return u;
+}
---
+++ Vector.hpp
@@ -0,0 +1,11 @@
+public class Vector {
+
+  private:
+    float x;
+    float y;
+    float z;
+
+  public:
+    Vector operator+(const Vector& v) const;
+
+}
